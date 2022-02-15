4b6ad54b3bdb8e4749053306139e6cd7822283f2
Pooja Modi <pooja18modi@gmail.com>
2022-02-15 17:46:35 +0530

    feat: spring app

--- src/com/springapp/HelloStringApp.java
+++ src/com/springapp/HelloStringApp.java
@@ -6,11 +6,8 @@ public class HelloStringApp {
     public static void main(String[] args) {
         ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext("springConfig.xml");
 
-        Coach baseBallCoach = context.getBean("BaseBall",Coach.class);
-        System.out.println(baseBallCoach.getDailyWorkout());
-
-        Coach trackCoach = context.getBean("TrackCoach",Coach.class);
-        System.out.println(trackCoach.getDailyWorkout());
+        Coach coach = context.getBean("TrackCoach",Coach.class);
+        System.out.println(coach.getDailyWorkout());
 
         context.close();
     }
